8b2747b1d83d53586691da35beaa1f8c89fce152
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-09-20 14:46:57 +0300

    more functions. name, age, describe, person

--- script.js
+++ script.js
@@ -1,66 +1,37 @@
-function hello(name = 'Nimetu', age=Infinity){
-    let yearText = 'years';
-    if(age === 1){
-        yearText = 'year';
-    }
-    // console.log('Hello ' + name + '. You are ' + age + ' years old.');
-    console.log(`Hello ${name}. You are ${age} ${yearText} old`);
+function hello(){
+    console.log('hello');
+}
+let hello2 = function(){
+    console.log('hello2');
+}
+let hello3 = () => {
+    console.log('hello3');
 }
 
-hello('Magnus',17);
-hello('Nigel',128);
-hello('Raul',1);
+let hello4 = () => console.log('hello4');
+
 hello();
+hello2();
+hello3();
+hello4();
 
-function volume(h, l, w){
-    return h*w*l;
-}
-
-let answer = volume(4,2,5);
-console.log(answer);
-
-function numbers(i) {
-    console.log(i);
-    if(i<10){
-        numbers(i+1);
+let person = {
+    name: 'Mag',
+    age: '17',
+    describe(){
+        console.log('name: ' + this.name + ' age: ' + this.age);
+    },
+    describe2: () => {
+        console.log('name: ' + this.name + ' age: ' + this.age);
+    },
+    describe3: function () {
+        console.log('name: ' + this.name + ' age: ' + this.age);
     }
 }
 
-numbers(0);
+person.name = 'Marvin';
+person.age = 43;
 
-let nums = [0,1,2,3,4];
-
-function print(num){
-    console.log(num);
-}
-
-nums.forEach(print);
-nums.forEach(num => { //usually this would be nums.forEach(function (num) {..., but since we have a single modifier, then we can shorten it.
-    console.log(`Num is ${num}`)
-});
-nums.forEach(num => console.log(`Num is ${num}`)); //shortest version
-
-let sums = nums.map(num => {
-    return num + 10;
-});
-console.log(sums);
-
-sums = nums.map(num => num + 10); //doesn't require return if one line
-console.log(sums);
-
-let sum = nums.reduce((total, num) => total + num);
-console.log(sums);
-
-let sorted = nums.sort((a,b) => {
-    if(a > b) {
-        return -1; //when 1 and -1 are flipped, array counts from other end
-    } else if(a < b) {
-        return 1;
-    } else {
-        return 0;
-    }
-});
-console.log(sorted);
-
-console.log(Math.PI);
-console.log(Math.round(Math.random() *50) + 50);
+person.describe();
+person.describe2();
+person.describe3();
