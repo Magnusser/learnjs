7cecfd8b0ae9731a9e297fc64820e81582c31e03
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-09-20 16:00:00 +0300

    eplilepsy warning

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-function hello(){
-    console.log('hello');
-}
-let hello2 = function(){
-    console.log('hello2');
-}
-let hello3 = () => {
-    console.log('hello3');
-}
-
-let hello4 = () => console.log('hello4');
-
-hello();
-hello2();
-hello3();
-hello4();
-
-let person = {
-    name: 'Mag',
-    age: '17',
-    describe(){
-        console.log('name: ' + this.name + ' age: ' + this.age);
-    },
-    describe2: () => {
-        console.log('name: ' + this.name + ' age: ' + this.age);
-    },
-    describe3: function () {
-        console.log('name: ' + this.name + ' age: ' + this.age);
+//console.log(window, {document});
+let title = document.getElementById('title');
+console.log({title});
+setTimeout(() =>{
+    title.innerText = 'Goodbye JS';
+}, 5_000);
+setTimeout(() =>{
+    title.innerText = 'Hubbbbaaaaaa';
+    title.style.background = 'coral'
+}, 7_000);
+setTimeout(() =>{
+    title.innerText = 'Goooooodddbyeeeeeeeeee Java Scripture';
+    title.classList.add('green');
+}, 7_500);
+let r, g, b;
+setInterval(() =>{
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    document.body.style.background = `rgb(${r}, ${g}, ${b})`;
+    if(title.classList.contains('green')) {
+        title.classList.replace('green', 'red');
+    } else {
+        title.classList.replace('red', 'green');
     }
-}
+}, 1000);
 
-person.name = 'Marvin';
-person.age = 43;
-
-person.describe();
-person.describe2();
-person.describe3();
+let textinput = document.querySelector('#textinput');
+textinput.addEventListener('input', evt => {
+    console.log(evt);
+    title.innerText = textinput.value.split('').reverse().join('');
+});
