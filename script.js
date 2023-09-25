59f819c688da0729f614f7b1d369cd3c7a3e4a97
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-09-25 10:30:49 +0300

    Tunnitöö (flag, star and pie chart)

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-// console.log(window, {document});
-// setTimeout(() => {
-//     document.location = 'https://google.com';
-// }, 5000);
+
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
+// Flag
+ctx.rect(20,15,50,20);
+ctx.fillStyle = 'blue';
+ctx.fillRect(20,15,50,20);
+ctx.rect(20,35,50,20);
+ctx.fillStyle = 'black';
+ctx.fillRect(20,35,50,35);
+ctx.rect(20,55,50,20);
+ctx.fillStyle = 'white';
+ctx.fillRect(20,55,50,20);
+ctx.stroke();
+// Star
+ctx.beginPath(); 
+ctx.moveTo(100,55);
+ctx.lineTo(120,70);
+ctx.lineTo(110,50);
+ctx.lineTo(100,70);
+ctx.lineTo(120,55);
+ctx.lineTo(100,55);
+ctx.stroke();
+// Pie
+ctx.beginPath();
+ctx.arc(110,100, 20, 0, 2 * Math.PI);
+ctx.lineWidth = 1;
+ctx.fillStyle = 'black'; //this will make sense later
+ctx.fill();
+ctx.stroke();
+// Slice
+ctx.beginPath();
+ctx.lineWidth = 1;
+ctx.arc(110,100, 20, 5, 10);
+ctx.lineTo(110,100);
+ctx.lineTo(110,80);
+ctx.fillStyle = 'white'; //goes on top of the *first* one, so it looks all the same
+ctx.fill();
 ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(100,100,50,0,2 * Math.PI);
-ctx.strokeStyle = 'red'
-ctx.stroke();
-ctx.lineWidth = 3;
-ctx.stroke();
-ctx.font = 'Arial';
